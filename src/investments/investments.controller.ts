17bb7dc1f0c5041e4d9f1322e8f36ba09a3864ca
enymartins <enyalvesmartins@gmail.com>
2024-07-13 15:56:10 -0400

    feat: add swagger

--- src/investments/investments.controller.ts
+++ src/investments/investments.controller.ts
@@ -1,18 +1,22 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { AuthGuard } from 'src/auth/auth.guard'
 import { Investment } from 'src/db/entities/investment.entity'
 import { CreateInvestmentDto } from './dtos/create-investment.dto'
 import { InvestmentsService } from './investments.service'
 @UseGuards(AuthGuard)
+@ApiTags('investments')
 @Controller('investments')
 export class InvestmentsController {
   constructor(private readonly investmentsService: InvestmentsService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Criar Investimento' })
   async createInvestment(
     @Body() createInvestmentDto: CreateInvestmentDto,
-    userId: string,
+    @Req() req,
   ): Promise<Investment> {
+    const userId = req.user.sub
     return this.investmentsService.create(createInvestmentDto, userId)
   }
 }
